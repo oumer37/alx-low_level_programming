26d181b18ec6323e2fb72073c5bc311651f4ad37
root <oumich37@gmail.com>
2022-03-15 10:15:39 -0700

    l

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
 #include "main.h"
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+* main - prints the string "_putchar" from a character array.
+* Return: 0 on success.
 */
 int main(void)
 {
-char *sh = "Holberton";
+char c[] = "_putchar";
+int i;
 
-while (*sh)
+for (i = 0; i < 9; i++)
 {
-_putchar(*sh);
-sh++;
+_putchar(c[i]);
 }
 _putchar('\n');
-
 return (0);
 }
 
