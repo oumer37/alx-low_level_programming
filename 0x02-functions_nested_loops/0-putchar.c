dc505166784ab42ac3e6b721bb4fe704b1f8487a
root <oumich37@gmail.com>
2022-03-15 12:12:52 -0700

    xc

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,7 @@
 *
 * Return: Always 0 (Success)
 */
-int main(void)
-  
+int main(void)  
 {
 _putchar('_');
 _putchar('p');
@@ -17,7 +16,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
- 
 return (0);
 }
 
